ffdfdc88446cbdfb30bd49dd10cbd67d63f2a9fc
vivanwicaksono <vivaneko123@gmail.com>
2020-07-02 11:51:34 +0700

    Create photo thumbnail and post detail view

--- SinglePageApplication/Scripts/angularfunction.js
+++ SinglePageApplication/Scripts/angularfunction.js
@@ -9,10 +9,7 @@ app.factory("PostService", function ($http) {
     }
 
     function getPost() {
-        var sUrl = "https://jsonplaceholder.typicode.com/posts";
-        if (iUserID != "0") {
-            sUrl = "https://jsonplaceholder.typicode.com/posts?userId=" + iUserID;
-        }
+        sUrl = "https://jsonplaceholder.typicode.com/posts?userId=" + iUserID;
         var promises = $http({
             method: 'get',
             url: sUrl,
@@ -38,10 +35,8 @@ app.factory("AlbumService", function ($http) {
     }
 
     function getAlbum() {
-        var sUrl = "https://jsonplaceholder.typicode.com/albums";
-        if (iUserID != "0") {
-            sUrl = "https://jsonplaceholder.typicode.com/albums?userId=" + iUserID;
-        }
+        sUrl = "https://jsonplaceholder.typicode.com/albums?userId=" + iUserID;
+
         var promises = $http({
             method: 'get',
             url: sUrl,
@@ -55,6 +50,69 @@ app.factory("AlbumService", function ($http) {
     return {
         setAlbum: setAlbum,
         getAlbum: getAlbum
+    };
+});
+
+//service for photos
+app.factory("PhotoService", function ($http) {
+    var iAlbumID = '0'
+
+    function setPhoto(pAlbumID) {
+        iAlbumID = pAlbumID;
+    }
+
+    function getPhoto() {
+        var sUrl = "";
+        sUrl = "https://jsonplaceholder.typicode.com/photos?albumId=" + iAlbumID;
+
+        var promises = $http({
+            method: 'get',
+            url: sUrl,
+        });
+        promises.then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            var errDetail = 'error:' + error.status;
+            //throw errors for chaining
+            throw errDetail;
+        });
+        return promises;
+    }
+
+    return {
+        setPhoto: setPhoto,
+        getPhoto: getPhoto
+    };
+});
+
+//service for Post detail
+app.factory("PostDetailService", function ($http) {
+    var iPostID = '0';
+
+    function setPostDetail(pPostID) {
+        iPostID = pPostID;
+    }
+
+    function getPostDetail() {
+        var sUrl = "";
+        sUrl = "https://jsonplaceholder.typicode.com/posts/" + iPostID;
+
+        var promises = $http({
+            method: 'get',
+            url: sUrl,
+        });
+        promises.then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            var errDetail = 'error:' + error.status;
+            throw errDetail;
+        });
+        return promises;
+    }
+
+    return {
+        setPostDetail: setPostDetail,
+        getPostDetail: getPostDetail
     };
 });
 
@@ -77,21 +135,50 @@ app.controller('userCtrl', function ($scope, $rootScope, $http, PostService, Alb
     }
 });
 
-app.controller('postCtrl', function ($scope, $rootScope, $http, PostService) {
+app.controller('postCtrl', function ($scope, $rootScope, $http, PostService, PostDetailService) {
     $scope.$watch('show', function () {
         $scope.posts = [];
         PostService.getPost().then(function (value) {
             $scope.posts = value.data;
         });
     });
+
+    $scope.viewPostDetail = function (pPostID) {
+        PostDetailService.setPostDetail(pPostID);
+        $rootScope.show = 'postdetail';
+    }
 });
 
-app.controller('albumCtrl', function ($scope, $rootScope, $http, AlbumService) {
+app.controller('postDetailCtrl', function ($scope, $rootScope, $http, PostService, PostDetailService) {
+    $scope.$watch('show', function () {
+        $scope.postdetail = [];
+        PostDetailService.getPostDetail().then(function (value) {
+            $scope.postdetail = value.data;
+        }).catch(function (error) {
+            var errDetail = 'error:' + error.status;
+        });
+    });
+});
+
+app.controller('albumCtrl', function ($scope, $rootScope, $http, AlbumService, PhotoService) {
     $scope.$watch('show', function () {
         $scope.albums = [];
         AlbumService.getAlbum().then(function (value) {
             $scope.albums = value.data;
         });
+    });
+    $scope.viewPhoto = function (pAlbumID) {
+        PhotoService.setPhoto(pAlbumID);
+        $rootScope.show = 'photo';
+    }
+});
+
+app.controller('photoCtrl', function ($scope, $rootScope, $http, PhotoService) {
+    $scope.$watch('show', function () {
+        $scope.photos = [];
+        PhotoService.getPhoto().then(function (value) {
+            $scope.photos = value.data;
+         });
     });
 });
 
